1214c26f8d2185247dc45ca21446d0c830b54c30
rabobahago <rabobahago@gmail.com>
2023-08-15 08:38:24 +0100

    portfolio, contact, about and home pages modified

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,10 +5,34 @@ export default function Footer() {
     <div className={styles.container}>
       <div>©2023 Rabodev. All rights reserved.</div>
       <div className={styles.social}>
-        <Image src="/1.png" width={15} height={15} alt="Rabodev facebook" />
-        <Image src="/2.png" width={15} height={15} alt="Rabodev instagram" />
-        <Image src="/3.png" width={15} height={15} alt="Rabodev twitter" />
-        <Image src="/4.png" width={15} height={15} alt="Rabodev youtube" />
+        <Image
+          src="/1.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Rabodev facebook"
+        />
+        <Image
+          src="/2.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Rabodev instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Rabodev twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={styles.icon}
+          alt="Rabodev youtube"
+        />
       </div>
     </div>
   );
